998afe996f6537cbc0b8f642c2fb4f703443de47
lpmi-13 <leskis@gmail.com>
2019-01-23 07:37:50 +0000

    add favicon

--- mysite/stats/urls.py
+++ mysite/stats/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 import urllib.parse
 
 BASE_API_PATH = 'api/'
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path(urllib.parse.urljoin(BASE_API_PATH, 'unique_commits_stats/'), views.UniqueCommitsUsersListCreate.as_view(), name='unique-commits' ),
     path(urllib.parse.urljoin(BASE_API_PATH, 'total_commits_stats/'), views.TotalCommitsUsersListCreate.as_view(), name='total-commits' ),
     path(urllib.parse.urljoin(BASE_API_PATH, 'search_users/'), views.SearchUsersCreate.as_view(), name='search-users' ),
-    # path(urllib.parse.urljoin(BASE_API_PATH, 'user/:username', views.SingleUser.as_view(), name='single-user'))
+    path('favicon.ico', favicon_view)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
