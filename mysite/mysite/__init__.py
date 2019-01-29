bab456dd4682a2cfa2284ffe49c931c8cc84ae23
lpmi-13 <leskis@gmail.com>
2019-01-29 05:55:17 +0000

    remove wip addons

--- mysite/mysite/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-# always import when Django starts
-from .celery import app as celery_app
--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -101,31 +101,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-# CELERY CONFIGS
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-
-# CACHE SETTINGS FOR THROTTLING
-CACHES = {
-  'default': {
-    'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-    'LOCATION': '127.0.0.1.11211',
-  }
-}  
-
 # REST FRAMEWORK CONFIGS
 REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_CLASSES': {
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle',
         'mysite.throttling.CustomThrottle',
-    },
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '1/min',
-        'user': '1/min'
     }
 }
