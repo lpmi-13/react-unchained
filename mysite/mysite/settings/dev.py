2c24d3695514ffd1e5d04316c440e28bb4ce77af
lpmi-13 <leskis@gmail.com>
2019-02-20 18:57:58 +0000

    update with specific docker config file for django

--- mysite/mysite/settings/dev.py
+++ mysite/mysite/settings/dev.py
@@ -1,14 +1,20 @@
 from mysite.settings.base import *
+import os
+import yaml
+
+with open('mysite/settings/db_config.yml', 'r') as inputfile:
+    cfg = yaml.load(inputfile)
 
 DEBUG = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db', # set in docker-compose.yml
-        'PORT': 5432 # default postgres port
+        'NAME': '{}'.format(cfg['DB_NAME']),
+        'USER': '{}'.format(cfg['DB_USER']),
+        'PASSWORD': '{}'.format(cfg['DB_PASSWORD']),
+        'HOST': '{}'.format(cfg['HOST']),
+        'PORT': '',
     }
 }
 
---
+++ mysite/mysite/settings/docker.py
@@ -0,0 +1,15 @@
+from mysite.settings.base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
+    }
+}
+
+ALLOWED_HOSTS = []
